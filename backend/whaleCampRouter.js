cd11678efb40c01366b63782921316fae6200c5f
MeepoohVP <poohvp1147@gmail.com>
2024-07-06 23:39:07 +0700

    set up package

--- backend/whaleCampRouter.js
+++ backend/whaleCampRouter.js
@@ -2,12 +2,28 @@ const exp = require('express');
 const app = exp();
 const port = 4006;
 const cors = require('cors');
-
+const mysql = require('mysql');
+require('dotenv').config();
+const dbconnect = mysql.createConnection({
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: process.env.DBDATABASE,
+    port: process.env.DBPORT
+});
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello from our server!');
 });
+
+app.get("/user",(req,res)=>{
+    const q="SELECT * FROM user"
+    dbconnect.query(q,(err,data)=>{
+        if(err) throw err;
+        res.json(data);
+    })
+})
 
 app.listen(port, '10.4.53.25', () => {
     console.log('server listening on port 8080')
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,33 @@
 import axios from 'axios';
 import './App.css';
 import './index.css';
+import { useState } from 'react';
 
-const apiCall = () => {
-  axios.get('http://10.4.53.25:4006/').then((data) => {
-    //this console.log will be in our frontend console
-    console.log(data.data)
-  })
-}
 
 function App() {
-
+  const [userList, setUserList] = useState([]);
+  const apiCall = () => {
+    axios.get('http://10.4.53.25:4006/user').then((res) => {
+      //this console.log will be in our frontend console
+      setUserList(res.data);
+    })
+  }
   return (
     <>
-      {/* <div className="App">
+      <div className="App">
       <header className="App-header">
 
-        <button onClick={apiCall} className="btn-secondary btn">Make API Call</button>
-
+        <button onClick={apiCall} className={`btn btn-primary text-red-300`}>Make API Call</button>
       </header>
-    </div> */}
+      {userList.map((val, key) => {
+        return (
+          <>
+          <p>{val.userId}</p>
+          <p>{val.userName}</p>
+          </>
+        )
+      })}
+    </div>
     
     </>
   )
